43a7d6ff87f37dc5629f52f03a3f4a6e9343e05e
ntkb4 <ntkjasur@gmail.com>
2021-10-02 10:48:14 -0400

    added subNum()

--- src/saturday/DemoSunday.java
+++ src/saturday/DemoSunday.java
@@ -1,6 +1,11 @@
 package saturday;
 
 public class DemoSunday {
+	
+	//Fahim..
+	public static void subNum(int a, int b) {
+		System.out.println(a-b);
+	}
 
 	public static void main(String[] args) {
 		
